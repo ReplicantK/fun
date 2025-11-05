f6156018d44df85eb666601072de57454e6a6ee0
ReplicantK <maldonadokeldin@gmail.com>
2025-11-05 08:22:06 -0800

    turn off image optimization for next config
    image optimization needs to be off because the static output setting:
    output: "export"

--- qr/next.config.ts
+++ qr/next.config.ts
@@ -2,7 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  distDir: "dist"
+  distDir: "dist",
+
+  images: {
+    unoptimized: true
+  }
 };
 
 export default nextConfig;
