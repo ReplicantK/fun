eb6e37eee50df1a01344ffc4656052a709b67709
ReplicantK <maldonadokeldin@gmail.com>
2026-02-28 23:40:52 -0800

    go: finish hello world

--- go/go_hello_world/hello.go
+++ go/go_hello_world/hello.go
@@ -2,16 +2,34 @@ package main
 
 import "fmt"
 
-const englishHelloPrefix = "Hello, "
+const (
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix = "Bonjour, "
+	latinHelloPrefix = "Salve, "
 
-func Hello(name string) string {
+	spanish = "Spanish"
+	french = "French"
+	latin = "Latin"
+)
+
+func Hello(name string, lang string) string {
 	if name == "" {
-		return englishHelloPrefix + "World"
+		name = "World"
+	} 
+
+	switch lang {
+		case spanish:
+			return spanishHelloPrefix + name
+		case french:
+			return frenchHelloPrefix + name
+		case latin:
+			return latinHelloPrefix + name
 	}
 
 	return englishHelloPrefix + name
 }
 
 func main() {
-	fmt.Println(Hello("World"))
+	fmt.Println(Hello("World", ""))
 }
--- go/go_hello_world/hello_test.go
+++ go/go_hello_world/hello_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("saying hello to a name when a string is passed in", func(t *testing.T) {
-		got := Hello("Chris")
+		got := Hello("Chris", "")
 		want := "Hello, Chris"
 
 		if got != want {
@@ -13,8 +13,35 @@ func TestHello(t *testing.T) {
 	})
 
 	t.Run("saying 'Hello, World' when no string is passed in", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
+
+		if got != want {
+			t.Error("got:", got, "want:", want)
+		}
+	})
+
+	t.Run("testing greetings in spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+
+		if got != want {
+			t.Error("got:", got, "want:", want)
+		}
+	})
+
+	t.Run("return greetings in french", func(t *testing.T) {
+		got := Hello("Chris", "French")
+		want := "Bonjour, Chris"
+
+		if got != want {
+			t.Error("got:", got, "want:", want)
+		}
+	})
+
+	t.Run("return greetings in latin", func(t *testing.T) {
+		got := Hello("Mike", "Latin")
+		want := "Salve, Mike"
 
 		if got != want {
 			t.Error("got:", got, "want:", want)
