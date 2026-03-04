c7e603089f1266d1c437c5d0f16c2bd359e33994
ReplicantK <maldonadokeldin@gmail.com>
2026-03-04 04:57:33 -0800

    go: complete arrays and slices

--- learn_go_with_tests/arrays_slices/sum.go
+++ learn_go_with_tests/arrays_slices/sum.go
@@ -9,3 +9,28 @@ func Sum(nums []int) int {
 
 	return sum
 }
+
+func SumAll(slices ...[]int) []int {
+	var result []int
+
+	for _, slice := range slices {
+		result = append(result, Sum(slice))
+	}
+
+	return result
+}
+
+func SumAllTails(slices ...[]int) []int {
+	var sums []int
+
+	for _, slice := range slices {
+		if len(slice) == 0 {
+			sums = append(sums, 0)
+		} else {
+			current_tail := slice[1:]
+			sums = append(sums, Sum(current_tail))
+		}
+	}
+
+	return sums
+}
--- learn_go_with_tests/arrays_slices/sum_test.go
+++ learn_go_with_tests/arrays_slices/sum_test.go
@@ -1,6 +1,7 @@
 package main
 
 import "testing"
+import "slices"
 
 func TestSum(t *testing.T) {
 	nums := []int{1, 2, 3, 4, 5}
@@ -11,4 +12,36 @@ func TestSum(t *testing.T) {
 	if got != want {
 		t.Error("got:", got, "expected:", want)
 	}
+}
+
+func TestSumAll(t *testing.T) {
+	slice1 := []int{1, 2}
+	slice2 := []int{0, 9}
+
+	expected := []int{3, 9}
+	got := SumAll(slice1, slice2)
+
+	if !slices.Equal(expected, got) {
+		t.Error("expected:", expected, "got:", got)
+	}
+}
+
+func TestSumTails(t *testing.T) {
+	t.Run("test non empty slices", func(t *testing.T) {
+		expected := []int{2, 9}
+		got := SumAllTails([]int{1, 2}, []int{0, 9})
+
+		if !slices.Equal(expected, got) {
+			t.Error("expected:", expected, "got:", got)
+		}
+	})
+
+	t.Run("test empty slice", func(t *testing.T) {
+		expected := []int{0, 9}
+		got := SumAllTails([]int{}, []int{3, 4, 5})
+
+		if !slices.Equal(got, expected) {
+			t.Error("expected:", expected, "got:", got)
+		}
+	})
 }
