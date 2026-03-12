d3762d8b639809f8ed442a544a9282f9278520dd
ReplicantK <maldonadokeldin@gmail.com>
2026-03-11 17:14:32 -0700

    go: complete dependency injection section

---
+++ learn_go_with_tests/dependency_injection/di.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+import "io"
+import "net/http"
+import "log"
+
+func Greet(writer io.Writer, name string) {
+	fmt.Fprintf(writer, "Hello, %s", name)
+}
+
+func MyGreeterHandler(w http.ResponseWriter, r *http.Request) {
+	Greet(w, "world")
+}
+
+func main() {
+	log.Fatal(http.ListenAndServe(":5001", http.HandlerFunc(MyGreeterHandler)))
+}
