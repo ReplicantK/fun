fef3383d0f05cd69c860d192cef0147ba76e5179
ReplicantK <maldonadokeldin@gmail.com>
2026-03-17 00:02:40 -0700

    go: mocking section 3, 2, 1 Go!

---
+++ learn_go_with_tests/mocking/countdown_test.go
@@ -0,0 +1,17 @@
+package main
+
+import "testing"
+import "bytes"
+
+func TestCountdown(t *testing.T) {
+	buffer := &bytes.Buffer{}
+
+	Countdown(buffer)
+
+	got := buffer.String()
+	want := "3\n2\n1\nGo!"
+
+	if got != want {
+		t.Error("expected:", want, "got:", got)
+	}
+}
